ec75db0c7ece1889de53a33ae065073345daedab
Manrique Recinos <manrique.recinos23@gmail.com>
2025-07-22 21:17:39 -0600

    enrutado de vista calculadora

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from '@/routes/Routes';
 import Layout from '@/components/Layout';
 
 export default function App() {
   return (
-    <Layout>
-      <Routes />
-    </Layout>
+    <Router>
+      <Layout>
+        <Routes />
+      </Layout>
+    </Router>
   );
 }
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Home from '@/pages/Home';
+import CalculatorPage from '@/pages/CalculatorPage';
 
 export default function Routes() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/' component={Home} />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route path='/calculator' component={CalculatorPage} />
+    </Switch>
   );
 }
